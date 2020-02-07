ffc8e07ec0aea887b4fb2b0fa1bd4c1e06444362
Liza <liza79682@gmail.com>
2020-02-07 15:46:55 +0200

    update log & secret key

--- src/main/java/task/NewOpportunityPortal/cryp/EncryptDecrypt.java
+++ src/main/java/task/NewOpportunityPortal/cryp/EncryptDecrypt.java
@@ -11,7 +11,7 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
@@ -19,23 +19,23 @@ import java.security.NoSuchAlgorithmException;
 @Component
 public class EncryptDecrypt {
 
-    private static final String SECRET_KEY = "f789054kjl";
+    private static final String SECRET_KEY="${secret.key}";
 
     private SecretKeySpec secretKeySpec;
     private Cipher cipher;
 
 
-    public String encrypt(String toBeEncrypt) throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
+    public String encrypt(String toBeEncrypt) throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
         cipher = Cipher.getInstance("AES");
-        secretKeySpec = new SecretKeySpec(SECRET_KEY.getBytes("UTF-8"), "AES");
+        secretKeySpec = new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), "AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
         byte[] encrypted = cipher.doFinal(toBeEncrypt.getBytes());
         return Base64.encodeBase64String(encrypted);
     }
 
-    public String decrypt(String encrypted) throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException {
+    public String decrypt(String encrypted) throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
         cipher = Cipher.getInstance("AES");
-        secretKeySpec = new SecretKeySpec(SECRET_KEY.getBytes("UTF-8"), "AES");
+        secretKeySpec = new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), "AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
         byte[] decryptedBytes = cipher.doFinal(Base64.decodeBase64(encrypted));
         return new String(decryptedBytes);
--- src/main/java/task/NewOpportunityPortal/service/impl/MessageServiceImpl.java
+++ src/main/java/task/NewOpportunityPortal/service/impl/MessageServiceImpl.java
@@ -12,10 +12,8 @@ import org.springframework.stereotype.Service;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 import java.util.Date;
 
 @Service
@@ -34,8 +32,8 @@ public class MessageServiceImpl implements MessageService{
         try {
             log.info("Encoder message");
             message.setText(encryptDecrypt.encrypt(message.getText()));
-        } catch (BadPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | UnsupportedEncodingException e) {
-            log.error(Arrays.toString(e.getStackTrace()));
+        } catch (BadPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException  e) {
+            log.error("Error during text encryption: ", e);
         }
         log.info("Create message: {}", message.getId());
         return repository.createMessage(message);
@@ -45,12 +43,11 @@ public class MessageServiceImpl implements MessageService{
     public Message getMessage(Long messageId) {
         log.info("Get message: {}", messageId);
         Message message = repository.getMessage(messageId);
-        log.info("Encoder message");
         try {
-            log.info("Encoder message");
+            log.info("Decoder message");
             message.setText(encryptDecrypt.decrypt(message.getText()));
-        } catch (BadPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException | UnsupportedEncodingException e) {
-            log.error(Arrays.toString(e.getStackTrace()));
+        } catch (BadPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException  e) {
+            log.error("Error during text decryption: ", e);
         }
         return message;
     }
